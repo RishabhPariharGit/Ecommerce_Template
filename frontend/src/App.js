1f342ae0d95a15aa9eff7f29f75bdcb3fd18a0b3
Rishabh <rishabhgit1704@gmail.comin>
2024-06-04 17:47:38 +0530

    changed name of folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import AuthRoutes from './Routes/routes'
-// import LogIn from './components/auth/login/login'
-// import SignUp from './components/auth/signup/signup'
+import AuthRoutes from './Routes/authroutes'
 
 
 const App = () => {
