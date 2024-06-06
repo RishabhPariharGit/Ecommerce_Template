f3305b5fd67942f27f435f4d2c8a91accf89e860
Rishabh <rishabhgit1704@gmail.comin>
2024-06-06 17:07:34 +0530

    Updated the folder structure

--- frontend/src/website/Routes/authroutes.js
+++ frontend/src/website/Routes/authroutes.js
@@ -1,13 +1,15 @@
 // import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUp from "../components/auth/signup/signup";
-import LogIn from "../components/auth/login/login";
+import SignUp from "../web_components/auth/signup/signup";
+import LogIn from "../web_components/auth/login/login";
+import Home from "../pages/Home";
 
 export default function AuthRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LogIn />}/>
+      <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<LogIn />}/>
         <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
