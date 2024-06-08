c080dde75d55c2f7e7d8c36046be63157df64b0e
Pallavi2001Hanwat <Pallavihanwat000@gmail.com>
2024-06-08 14:28:25 +0530

    backend Code

--- backend/Database/Connection.js
+++ backend/Database/Connection.js
@@ -1,33 +1,33 @@
-// const mongoose = require("mongoose");
-// const db =
-//   "mongodb+srv://rishabhgit1704:Rentalmanagement123@rentalmgmt.qoi0sqo.mongodb.net/";
-// /* Replace <password> with your database password */
+const mongoose = require("mongoose");
+const db =
+  "mongodb+srv://rishabhgit1704:Rentalmanagement123@rentalmgmt.qoi0sqo.mongodb.net/";
+/* Replace <password> with your database password */
 
-// mongoose.set("strictQuery", true, "useNewUrlParser", true);
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(db);
-//     console.log("MongoDB is Connected...");
-//   } catch (err) {
-//     console.error("Unable to connect to MongoDB");
-//     process.exit(1);
-//   }
-// };
-// module.exports = connectDB;
-
-
-const mongoose = require('mongoose')
+mongoose.set("strictQuery", true, "useNewUrlParser", true);
 
 const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/Rentalmanagement`);
-      console.log(`MongoDB Connected: {conn.connection.host}`);
-    } catch (error) {
-      
-      console.error(error.message);
-    
-    }
+  try {
+    await mongoose.connect(db);
+    console.log("MongoDB is Connected...");
+  } catch (err) {
+    console.error("Unable to connect to MongoDB");
+    process.exit(1);
   }
+};
+module.exports = connectDB;
 
-  module.exports = connectDB;
+
+// const mongoose = require('mongoose')
+
+// const connectDB = async () => {
+//     try {
+//       const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/Rentalmanagement`);
+//       console.log(`MongoDB Connected: {conn.connection.host}`);
+//     } catch (error) {
+      
+//       console.error(error.message);
+    
+//     }
+//   }
+
+//   module.exports = connectDB;
