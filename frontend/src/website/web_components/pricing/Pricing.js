fb0066b82620aabd73959a69c710a479778ff0e5
Rishabh <rishabhgit1704@gmail.comin>
2024-06-10 18:18:56 +0530

    added banner and pricing sections

---
+++ frontend/src/website/web_components/pricing/Pricing.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import SingleCard from './Single-card'
+import './Single-Card.css'
+
+const Pricing = () => {
+  return (
+   <>
+  <div className='Card-wrapper'>
+<SingleCard/>
+<SingleCard/>
+<SingleCard/>
+</div>
+   </>
+  )
+}
+
+export default Pricing
