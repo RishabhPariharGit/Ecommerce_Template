43f65acf6c968245d5b47c107ad7a497bc0f1d42
sachin <rishabhgit1704@gmail.comin>
2024-06-12 18:20:40 +0530

    Added footer

--- frontend/src/website/pages/Home.js
+++ frontend/src/website/pages/Home.js
@@ -6,6 +6,7 @@ import AboutComp from '../web_components/aboutcomp/AboutComp'
 import ProductCardGrid from '../web_components/Productcard/ProductCardGrid/ProductCardGrid'
 import Newsletter from '../web_components/NewsLetter/Newsletter'
 import Testimonials from '../web_components/Testimonials/Testimonials'
+import Footer from '../web_components/Footer/Footer'
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
     <ProductCardGrid/>
     <Testimonials/>
     <Newsletter/>
+    <Footer/>
     </>
   )
 }
