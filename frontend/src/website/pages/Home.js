bd09f23e9b533dced5e8ae79a022aac3adb55da9
Rishabh <rishabhgit1704@gmail.comin>
2024-06-12 16:04:27 +0530

    Added the newsletter

--- frontend/src/website/pages/Home.js
+++ frontend/src/website/pages/Home.js
@@ -3,6 +3,8 @@ import Navbar from '../web_components/navbar/Navbar'
 import MainHomebanner from '../web_components/mainbanner/MainHomebanner'
 import Pricing from '../web_components/pricing/Pricing'
 import AboutComp from '../web_components/aboutcomp/AboutComp'
+import ProductCardGrid from '../web_components/Productcard/ProductCardGrid/ProductCardGrid'
+import Newsletter from '../web_components/NewsLetter/Newsletter'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
     <MainHomebanner/>
     <Pricing/>
     <AboutComp/>
+    <ProductCardGrid/>
+    <Newsletter/>
     </>
   )
 }
---
+++ frontend/src/website/web_components/Productcard/Productcard.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Productcard.css'
+
+const Productcard = () => {
+  return (<>
+   <div className='main-card-wrapper'>
+    <section>
+  <div className="product-card spacing">
+    <div className="badge">New Product</div>
+    <div className="product-thumb">
+      <img src="https://cdn-icons-png.flaticon.com/512/2806/2806251.png" />
+    </div>
+    <div className="product-details">
+      <span className="product-category">T-Shirt</span>
+      <h4>
+        <a href="#">New T-Shirt For Man</a>
+      </h4>
+      <p>
+        New Import T-Shirt For Man Very Rare Collection, If You Want Order Right
+        Now
+      </p>
+      <div className="product-bottom-details">
+        <div className="product-price">
+          <small>$15.10</small>$7.99
+        </div>
+        <div className="product-links">
+          <a href="#">
+            <i className="fa fa-heart" />
+          </a>
+          <a href="#">
+            <i className="fa fa-shopping-cart" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+   </div>
+   </>
+  )
+}
+
+export default Productcard
