60e253a35decd3a50d128ec8759a1e0fe24cb6e3
Rishabh <rishabhgit1704@gmail.comin>
2024-07-05 14:16:20 +0530

    added hover on navbar

--- frontend/src/website/web_components/navbar/Navbar.js
+++ frontend/src/website/web_components/navbar/Navbar.js
@@ -39,6 +39,7 @@ function classNames(...classes) {
 
 export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
   return (
     <header className="bg-white">
@@ -47,7 +48,7 @@ export default function Navbar() {
           <a href="/" className="customlogodsg -m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             {/* <img className="h-8 w-auto" src="https://previews.dropbox.com/p/thumb/ACXZyo_m0RqOxIDiP3iFX7mHQOUIl5VjpOqQWB1DbSR0N0dmME7VwN2jTZCB0V8h6_0udhMt-T13ChqWCp6zXM2sizCJMpXodvVfy-3tSAHKX4TCjMGeB5TK8MoSQuemOPFXyqM4UhbdrVmAPACDv4iC7vDYOqBkD6UhFwvAIgSaLB-kwNUDn9P5-KXaDia_PLxUwRyEbrq-IWma2HDavtzO94qFXwj_drZqEKhBPWgqS2GY5Z8TgWX5iGSZndbQq81bR5vVx-Q4nY_UA43_dRs6jynTC_agIvOr_5gAEAlstdtdOLEZzvqxNYfGEoJfOldZH8UG9FVt-ssMaUKoMvsM/p.png" alt="" /> */}
-          CUSTOMLOGO
+            CUSTOMLOGO
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -65,71 +66,77 @@ export default function Navbar() {
             HOME
           </a>
           <Popover className="relative">
-            <PopoverButton className=" navtextcustom ServicesBtnCstm flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-              SERVICES
-              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-            </PopoverButton>
-
-            <Transition
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
+            <div
+              onMouseEnter={() => setIsPopoverOpen(true)}
+              onMouseLeave={() => setIsPopoverOpen(false)}
             >
-              <PopoverPanel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
-                      </div>
-                      <div className="flex-auto">
-                        {item.to ? (
-                          <Link to={item.to} className="block font-semibold text-gray-900">
-                            {item.name}
-                            <span className="absolute inset-0" />
-                          </Link>
-                        ) : (
-                          <a href={item.href} className="block font-semibold text-gray-900">
-                            {item.name}
-                            <span className="absolute inset-0" />
-                          </a>
-                        )}
-                        <p className="mt-1 text-gray-600">{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                  {callsToAction.map((item) => (
-                    item.to ? (
-                      <Link
+              <PopoverButton className="navtextcustom ServicesBtnCstm flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+                SERVICES
+                <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+              </PopoverButton>
+
+              <Transition
+                show={isPopoverOpen}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <PopoverPanel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                  <div className="p-4">
+                    {products.map((item) => (
+                      <div
                         key={item.name}
-                        to={item.to}
-                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
                       >
-                        <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-                        {item.name}
-                      </Link>
-                    ) : (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                      >
-                        <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-                        {item.name}
-                      </a>
-                    )
-                  ))}
-                </div>
-              </PopoverPanel>
-            </Transition>
+                        <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                          <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                        </div>
+                        <div className="flex-auto">
+                          {item.to ? (
+                            <Link to={item.to} className="block font-semibold text-gray-900">
+                              {item.name}
+                              <span className="absolute inset-0" />
+                            </Link>
+                          ) : (
+                            <a href={item.href} className="block font-semibold text-gray-900">
+                              {item.name}
+                              <span className="absolute inset-0" />
+                            </a>
+                          )}
+                          <p className="mt-1 text-gray-600">{item.description}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                    {callsToAction.map((item) => (
+                      item.to ? (
+                        <Link
+                          key={item.name}
+                          to={item.to}
+                          className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                        >
+                          <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                          {item.name}
+                        </Link>
+                      ) : (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                        >
+                          <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                          {item.name}
+                        </a>
+                      )
+                    ))}
+                  </div>
+                </PopoverPanel>
+              </Transition>
+            </div>
           </Popover>
 
           <Link to="/about" className="navtextcustom text-sm font-semibold leading-6 text-gray-900">
