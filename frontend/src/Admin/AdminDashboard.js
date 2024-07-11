b2f8b2c41d8e39a529a1565738741879fe277796
Rishabh <rishabhgit1704@gmail.comin>
2024-07-11 11:26:36 +0530

    started creating admin main dashboard

---
+++ frontend/src/Admin/AdminDashboard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './AdminDashboard.css'
+
+
+const AdminDashboard = () => {
+  return (
+  <>
+  <div className='main-admin-wrapper'>
+    <div className='Side-Navigation'>
+    </div>
+    <div className='work-wrapper-main'>
+
+      <div className='main-profile-wrapper'>
+      <div className='secondary-profile-wrapper'>
+      </div>
+      </div>
+
+<div className='main-work-area-wrapper'>
+</div>
+    </div>
+  </div>
+  </>
+  )
+}
+
+export default AdminDashboard
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-import AuthRoutes from './website/Routes/authroutes'
-import Navbar from './website/web_components/navbar/Navbar'
-import Footer from './website/web_components/Footer/Footer'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import AuthRoutes from './website/Routes/authroutes';
+import Navbar from './website/web_components/navbar/Navbar';
+import Footer from './website/web_components/Footer/Footer';
 import './App.css';
 
-
 const App = () => {
+  const location = useLocation();
+  const hideHeaderFooterRoutes = ['/admin']; // Add any other routes you want to exclude header and footer
+
   return (
-   <>
-   <Navbar/>
- <AuthRoutes/>
- <Footer/>
-   </>
-  )
+    <>
+      {!hideHeaderFooterRoutes.includes(location.pathname) && <Navbar />}
+      <AuthRoutes />
+      {!hideHeaderFooterRoutes.includes(location.pathname) && <Footer />}
+    </>
+  );
 }
 
-export default App
+export default App;
