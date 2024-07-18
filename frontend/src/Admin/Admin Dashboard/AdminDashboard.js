2817c7b5f60a2ea4c570905b845bd7733e21e665
Rishabh <rishabhgit1704@gmail.comin>
2024-07-18 15:31:30 +0530

    added profile options

--- frontend/src/Admin/Admin Dashboard/AdminDashboard.js
+++ frontend/src/Admin/Admin Dashboard/AdminDashboard.js
@@ -128,6 +128,32 @@ const AdminDashboard = () => {
           <div className='main-profile-wrapper'>
             <div className='secondary-profile-wrapper'>
             </div>
+            <div className='secondary-profile-wrapper-two'>
+            <div className='profile-main-box'>
+            <div className='profile-main-box-desc'>             
+<p>Name:<span> Rishabh Singh Parihar</span></p><br/>
+<p>Age:<span> 23 Years</span></p><br/>
+<p>Designation:<span> Web Developer</span></p><br/>
+<p>Phone:<span> xx-yyyyy-zzzzz</span></p>
+
+<div className='dropdown'>
+              <button className="dropbtn custom-dropbtn-2"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+  <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+</svg>
+</button>
+              <div className="dropdown-content">
+                <a href="#">Profile</a>
+                <a href="#">Sign-out</a>
+              </div>
+            </div>
+
+
+</div>
+<div className='profile-img-wrapper'>
+  <img src='https://imageonline.co/imageclip.jpg'/>
+</div>
+</div> 
+            </div>
           </div>
           <div className='main-work-area-wrapper'>
           {showAdminPanel && <AdminPanel />}
@@ -139,3 +165,6 @@ const AdminDashboard = () => {
 }
 
 export default AdminDashboard;
+
+
+
