040e6317e9fbd3f9d38d1ac5db44cd5921cf9b4d
Rishabh <rishabhgit1704@gmail.comin>
2024-08-28 15:51:10 +0530

    added apis to home banner button

--- backend/Model/TextOverImageModel.js
+++ backend/Model/TextOverImageModel.js
@@ -5,7 +5,11 @@ const TextoverImageSchema= mongoose.Schema({
     Mainhead:String,
     Heading:String,
     Subhead:String,
-    Imgurl:String
+    Imgurl:String,
+    Buttonone:String,
+    linkone:String,
+    Buttontwo:String,
+    linktwo:String
 })
 const TextoverImage = mongoose.model( "TextOverImage", TextoverImageSchema);
 
