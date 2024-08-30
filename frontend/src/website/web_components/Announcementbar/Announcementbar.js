03f540fb83438acf2c44cb100322c32174ef4c54
Rishabh <rishabhgit1704@gmail.comin>
2024-08-30 17:22:44 +0530

    added slider to announcement bar

--- frontend/src/website/web_components/Announcementbar/Announcementbar.js
+++ frontend/src/website/web_components/Announcementbar/Announcementbar.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import './Announcementbar.css';
 
 const Announcementbar = () => {
-  return (<>
+  // Create refs for the navigation buttons
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
+  return (
     <div className='Announcement-main-wrapper'>
-        <p>Up to 50% off on selected products</p>
+      <Swiper
+        modules={[Navigation]}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        onBeforeInit={(swiper) => {
+          swiper.params.navigation.prevEl = prevRef.current;
+          swiper.params.navigation.nextEl = nextRef.current;
+        }}
+        loop={true}
+        className='swiper-container'
+      >
+        <SwiperSlide>
+          <p>Up to 50% off on selected products</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p>Free Shipping on orders above 500/-</p>
+        </SwiperSlide>
+      </Swiper>
+
+      {/* Custom Navigation Buttons */}
+      <div ref={prevRef} className="swiper-button-prev"></div>
+      <div ref={nextRef} className="swiper-button-next"></div>
     </div>
-    </>
-  )
+  );
 }
 
-export default Announcementbar
+export default Announcementbar;
