c8482df292766746845cc04864ea04d7c2378462
Rishabh <rishabhgit1704@gmail.comin>
2024-09-04 15:44:11 +0530

    added Breadcrumbs

--- frontend/src/website/pages/Product/MainProductpg.js
+++ frontend/src/website/pages/Product/MainProductpg.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './MainProductpg.css'
+import { Link } from 'react-router-dom'
+
 
 const MainProductpg = () => {
   return (
-    <div>MainProductpg</div>
+    <>
+    <div className='Product-pg-main-wrapper'>
+        <div className='accordian-main-wrapper'>
+          <Link to='/'>
+          <p>Home</p>
+          </Link>
+          <span>/</span>
+          <Link to = '/buyer'>
+          <p>Naruto-Collection</p>
+          </Link>
+          <span>/</span>
+          <p>Product 1</p>
+        </div>
+    </div>
+    </>
   )
 }
 
