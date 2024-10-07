762f7f9633fab5c7302b5e2bc0204cd90f5e9898
RishabhPariharGit <rishabhgit1704@gmail.com>
2024-10-08 03:14:42 +0530

    added styling to admin panel

--- frontend/src/Routes/WebsiteRoutes.js
+++ frontend/src/Routes/WebsiteRoutes.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SignUp from '../Shared/Auth/SignUp';
 import LogIn from '../Shared/Auth/Login';
-import Home from '../website/pages/Home';
-import About from '../Website/pages/About/About';
-import Blog from '../Website/pages/Blog/Blog'
-import Textoverimage from '../website/web_components/TextoverImage';
-import MainProductpg from '../Website/pages/Product/MainProductpg';
-import Contact from '../Website/pages/Contact/Contact';
+import Home from '../website/pages/Home/Home';
+import Blog from '../website/pages/Blog/Blog';
+import Textoverimage from '../website/web_components/TextoverImage/Textoverimage';
+import MainProductpg from '../website/pages/Product/MainProductpg';
+import Contact from '../website/pages/Contact/Contact';
+import About from '../website/pages/About/About';
 
 const WebsiteRoutes = () => {
   return (
@@ -17,7 +17,7 @@ const WebsiteRoutes = () => {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/product" element={<MainProductpg />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/about" element={<About/>} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/textoverimage" element={<Textoverimage />} />
     </Routes>
