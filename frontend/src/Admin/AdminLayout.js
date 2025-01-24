f7d46263286abe8c482d023b5ab0b0b6dc5bdbee
Pallavi2001Hanwat <Pallavihanwat000@gmail.com>
2025-01-24 22:21:18 +0530

    added stylling

--- frontend/src/Admin/AdminLayout.js
+++ frontend/src/Admin/AdminLayout.js
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Sidenav from './Sidenav';
 import './AdminStyle/AdminGlobalStyle.css';
 import Navbar from './AdminComponents/Navbar';
 
-
 const AdminLayout = ({ children }) => {
-    const [sidenavOpen, setSidenavOpen] = useState(window.innerWidth >= 768);
-
-    const toggleSidenav = () => {
-      setSidenavOpen((prev) => !prev);
-    };
+    
     return (
         <div className="body">
             <div className='wrapper' id='wrapper'>
-                <Sidenav isOpen={sidenavOpen} toggleSidenav={toggleSidenav}/>
-       
-                <div className="main-Content ">
-                <Navbar toggleSidenav={toggleSidenav} />
-              
+                <Sidenav />
+                <div className="main-Content">
+                <Navbar />
                     {children}
                 </div>
             </div>
--- frontend/src/Admin/Sidenav.js
+++ frontend/src/Admin/Sidenav.js
@@ -3,167 +3,156 @@ import './AdminStyle/AdminGlobalStyle.css';
 import Cookies from 'js-cookie';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faShoppingCart, faTags, faShirt, faBoxes, faUsers,faFileClipboard } from '@fortawesome/free-solid-svg-icons';
+import { faTachometerAlt, faShoppingCart, faTags, faBoxes, faUsers } from '@fortawesome/free-solid-svg-icons';
 import { getAllsections } from '../Services/HomepageSectionService';
 
-const Sidenav = ({ isOpen, toggleSidenav }) => {
-    const navigate = useNavigate();
-    const [sections, setSections] = useState([]);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isHovered, setIsHovered] = useState(false); // Track hover state
-    const isFetchedRef = useRef(false);
+const Sidenav = () => {
+  const navigate = useNavigate();
+  const [sections, setSections] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isHovered, setIsHovered] = useState(false); // Track hover state
+  const isFetchedRef = useRef(false);
 
-    const fetchSections = async () => {
-        try {
-            setIsLoading(true);
-            const response = await getAllsections(); // Fetch all sections from the service
-            setSections(response.data); // Assuming response.data contains an array of sections
-            setError(null); // Clear any previous error
-        } catch (err) {
-            console.error('Error fetching Sections:', err);
-            setError('Failed to fetch Sections. Please try again later.');
-        } finally {
-            setIsLoading(false); // Stop loading regardless of success or error
-        }
-    };
+  const fetchSections = async () => {
+    try {
+      setIsLoading(true);
+      const response = await getAllsections(); // Fetch all sections from the service
+      setSections(response.data); // Assuming response.data contains an array of sections
+      setError(null); // Clear any previous error
+    } catch (err) {
+      console.error('Error fetching Sections:', err);
+      setError('Failed to fetch Sections. Please try again later.');
+    } finally {
+      setIsLoading(false); // Stop loading regardless of success or error
+    }
+  };
 
-    useEffect(() => {
-        if (isHovered && !isFetchedRef.current) {
-            fetchSections();
-            isFetchedRef.current = true;
-        }
-    }, [isHovered]);
+  useEffect(() => {
+    if (isHovered && !isFetchedRef.current) {
+      fetchSections();
+      isFetchedRef.current = true;
+    }
+  }, [isHovered]);
 
-    const handleLogout = () => {
-        Cookies.remove('token');
-        Cookies.remove('role');
-        navigate('/login');
-    };
+  const handleLogout = () => {
+    Cookies.remove('token');
+    Cookies.remove('role');
+    navigate('/login');
+  };
 
-    return (
-      <>
-        <div className={`sidenav ${isOpen ? 'open' : ''}`}>
-
-            <div className="widedesktop">
-                <div className="top_navbar">
-                    <div className="hamburger">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                    <div className="top_menu">
-                        <div className="logo">
-                            <img id="logo" src="https://cdn.worldvectorlogo.com/logos/akasol-1.svg" alt="Logo" height="40" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="sidebar overflow-auto">
-                <div className="desktop">
-                    <div className="top_navbar">
-                        <div className="top_menu">
-                            <div className="logo center" style={{ paddingTop: '25px' }}>
-                                <img id="logo" src="https://cdn.worldvectorlogo.com/logos/akasol-1.svg" alt="Logo" height="45" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <ul style={{ borderTop: 'solid 1px #EEE' }}>
-                <li>
-    <Link to="/admin/dashboard">
-      <FontAwesomeIcon icon={faHouse} className="icon" />
-      <span className="title">Dashboard</span>
-    </Link>
-  </li>
-  <li>
-    <Link to="/admin/orders">
-      <FontAwesomeIcon icon={faShoppingCart} className="icon" />
-      <span className="title">Orders</span>
-    </Link>
-  </li>
-  <li>
-    <Link to="/admin/Category">
-      <FontAwesomeIcon icon={faTags} className="icon" />
-      <span className="title">Category</span>
-    </Link>
-  </li>
-  <li>
-    <Link to="/admin/SubCategory">
-      <FontAwesomeIcon icon={faBoxes} className="icon" />
-      <span className="title">SubCategory</span>
-    </Link>
-  </li>
-  <li>
-    <Link to="/admin/Products">
-      <FontAwesomeIcon icon={faShirt} className="icon" />
-      <span className="title">Products</span>
-    </Link>
-  </li>
-                    <li>
-                        <Link to="/admin/Pages">
-                            <FontAwesomeIcon icon={faFileClipboard} className="icon" />
-                            <span className="title">Pages</span>
-                        </Link>
-                    </li>
-
-                    <li>
-                        <Link to="/admin/Templates">
-                            <FontAwesomeIcon icon={faTags} className="icon" />
-                            <span className="title">Templates</span>
-                        </Link>
-                    </li>
-
-                    <li>
-    <div className="profile-dropdown-containerr">
-      <div
-        onClick={() => setIsHovered((prev) => !prev)} // Toggle dropdown on click
-        className="dropdown-toggle"
-      >
-        <Link to="/admin/HomepageSections">
-          <FontAwesomeIcon icon={faTags} className="icon" />
-          <span className="title">HomepageSections</span>
-        </Link>
-      </div>
-      <div className={`dropdown-menuuu ${isHovered ? "open" : ""}`}>
-        {isHovered && !isLoading && sections.length > 0 && (
-          <div className="dynamic-sections">
-            {sections.map((section) => (
-              <Link
-                key={section.id}
-                to={`/admin/HomepageSection/Edit/${section._id}`}
-                className="dropdown-itemmm"
-              >
-                <span className="dropdown-itemmm">{section.Title}</span>
-              </Link>
-            ))}
+  return (
+    <div>
+      <div className="widedesktop">
+        <div className="top-nav">
+          <div className="hamburger-btn">
+            <div></div>
+            <div></div>
+            <div></div>
           </div>
-        )}
-        {isLoading && <span className="dropdown-itemmm">Loading...</span>}
-        {error && <span className="dropdown-itemmm">{error}</span>}
+          <div className="top_menu">
+            <div className="logo">
+              <img id="logo" src="https://cdn.worldvectorlogo.com/logos/akasol-1.svg" alt="Logo" height="40" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="sidebar overflow-auto">
+        <div className="desktop">
+          <div className="top_navbar">
+            <div className="top_menu">
+              <div className="logo center" style={{ paddingTop: '25px' }}>
+                <img id="logo" src="https://cdn.worldvectorlogo.com/logos/akasol-1.svg" alt="Logo" height="45" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <ul style={{ borderTop: 'solid 1px #EEE' }}>
+          <li>
+            <Link to="/admin/dashboard">
+              <FontAwesomeIcon icon={faTachometerAlt} className="icon" />
+              <span className="title">Dashboard</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/orders">
+              <FontAwesomeIcon icon={faShoppingCart} className="icon" />
+              <span className="title">Orders</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/Category">
+              <FontAwesomeIcon icon={faTags} className="icon" />
+              <span className="title">Category</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/SubCategory">
+              <FontAwesomeIcon icon={faBoxes} className="icon" />
+              <span className="title">SubCategory</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/Products">
+              <FontAwesomeIcon icon={faTags} className="icon" />
+              <span className="title">Products</span>
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/Pages">
+              <FontAwesomeIcon icon={faTags} className="icon" />
+              <span className="title">Pages</span>
+            </Link>
+          </li>
+          <li>
+            <div className="profile-dropdown-containerr">
+              <div
+                onClick={() => setIsHovered((prev) => !prev)} // Toggle dropdown on click
+                className="dropdown-toggle"
+              >
+                <Link to="/admin/HomepageSections">
+                  <FontAwesomeIcon icon={faTags} className="icon" />
+                  <span className="title">HomepageSections</span>
+                </Link>
+              </div>
+              <div className={`dropdown-menuuu ${isHovered ? "open" : ""}`}>
+                {isHovered && !isLoading && sections.length > 0 && (
+                  <div className="dynamic-sections">
+                    {sections.map((section) => (
+                      <Link
+                        key={section.id}
+                        to={`/admin/HomepageSection/Edit/${section._id}`}
+                        className="dropdown-itemmm"
+                      >
+                        <span className="dropdown-itemmm">{section.Title}</span>
+                      </Link>
+                    ))}
+                  </div>
+                )}
+                {isLoading && <span className="dropdown-itemmm">Loading...</span>}
+                {error && <span className="dropdown-itemmm">{error}</span>}
+              </div>
+            </div>
+          </li>
+          <li>
+            <Link to="/admin/Users">
+              <FontAwesomeIcon icon={faUsers} className="icon" />
+              <span className="title">Users</span>
+            </Link>
+          </li>
+        </ul>
+
+        <div className="logout">
+          <div>
+            Logged in as <strong>User Name</strong>
+          </div>
+          <button className='btn logout-btn' onClick={handleLogout}>Logout</button>
+        </div>
       </div>
     </div>
-  </li>
-  <li>
-    <Link to="/admin/Users">
-      <FontAwesomeIcon icon={faUsers} className="icon" />
-      <span className="title">Users</span>
-    </Link>
-  </li>
-                </ul>
-
-                <div className="logout">
-                    <div>
-                        Logged in as <strong>User Name</strong>
-                    </div>
-                    <button className='btn logout-btn' onClick={handleLogout}>Logout</button>
-                </div>
-            </div>
-        </div>
-        </>
-    );
+  );
 };
 
 export default Sidenav;
